89be6b10b69bd7b99e59eae08b6be1262ab0f098
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-26 13:52:19 +0300

    Add redis and mongo class configs

---
+++ utils/db.js
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    this.host = process.env.DB_HOST || 'localhost';
+    this.port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+    this.url = `mongodb://${this.host}:${this.port}`;
+    this.connectionEstablished = false;
+    this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+    this.client
+      .connect()
+      .then(() => (this.connectionEstablished = true))
+      .catch((err) =>
+        console.log('Mongo client failed to connect:', err.toString())
+      );
+  }
+
+  isAlive() {
+    return this.connectionEstablished;
+  }
+
+  async nbUsers() {
+    return await this.client.db.collection('users').countDocuments();
+  }
+
+  async nbFiles() {
+    return await this.client.db.collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
---
+++ utils/redis.js
@@ -0,0 +1,38 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => {
+      this.connectionEstablished = false;
+      console.log('Redis client failed to connect:', err.toString());
+    });
+    this.connectionEstablished = false;
+    this.client.on('connect', () => (this.connectionEstablished = true));
+  }
+
+  isAlive() {
+    return this.connectionEstablished;
+  }
+
+  async get(key) {
+    return promisify(this.client.GET).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    return promisify(this.client.SET).bind(this.client)(
+      key,
+      value,
+      'Expiration',
+      duration
+    );
+  }
+
+  async del(key) {
+    return promisify(this.client.DEL).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
