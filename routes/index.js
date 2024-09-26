d4ba28d0f758b12cf222418f037bd195bed79f17
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-26 20:45:11 +0300

    Add auth controller and routes

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
