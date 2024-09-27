a110cb278cbbf11f2cde661ac71328d4e196077c
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-27 16:49:12 +0300

    Increase the limit of the request body

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import router from './routes/index';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
 app.use(router);
 
 app.listen(PORT, () => {
