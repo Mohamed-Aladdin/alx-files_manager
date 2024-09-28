8c7796878be97eb250bdc4fc4877acc6e82e5f29
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-28 22:28:55 +0300

    Fix lint errors

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -28,12 +28,17 @@ export default class AuthController {
     await redisClient.set(
       `auth_${key}`,
       fetchedUser._id.toString(),
-      60 * 60 * 24,
+      60 * 60 * 24
     );
     return res.status(200).json({ token: key });
   }
 
   static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
     const fetchedUser = await getUserByToken(req);
 
     if (!fetchedUser) {
