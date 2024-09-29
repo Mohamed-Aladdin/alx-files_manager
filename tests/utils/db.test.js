34a231c3c68d1507d3904ecc81e5a34831744645
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-29 15:06:33 +0300

    Add tests for db and redis clients

---
+++ tests/utils/db.test.js
@@ -0,0 +1,33 @@
+import { expect } from 'chai';
+import dbClient from '../../utils/db';
+
+describe('DBClient Class', () => {
+  before((done) => {
+    this.timeout(10000);
+    Promise.all([
+      dbClient.db().collection('users'),
+      dbClient.db().collection('files'),
+    ])
+      .then(([usersCollection, filesCollection]) => {
+        Promise.all([
+          usersCollection.deleteMany({}),
+          filesCollection.deleteMany({}),
+        ])
+          .then(() => done())
+          .catch((deleteErr) => done(deleteErr));
+      })
+      .catch((connectErr) => done(connectErr));
+  });
+
+  it('DBClient connectivity', () => {
+    expect(dbClient.isAlive()).to.be.true;
+  });
+
+  it('nbUsers returns the correct user count', () => {
+    expect(dbClient.nbUsers()).to.equal(0);
+  });
+
+  it('nbFiles returns the correct file count', () => {
+    expect(dbClient.nbFiles()).to.equal(0);
+  });
+});
