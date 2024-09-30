ecdb23a52ae881eba55a41413f2b501a17e7bacb
Mohamed Aladdin <moham3daladdin101@gmail.com>
2024-09-30 16:28:44 +0300

    Add more tests

--- tests/controllers/AuthController.test.js
+++ tests/controllers/AuthController.test.js
@@ -34,6 +34,24 @@ describe('Auth Controller', () => {
       .catch((connectErr) => done(connectErr));
   });
 
+  it('GET /connect with an invalid user or password', (done) => {
+    request.get(
+      `${URL}/connect`,
+      {
+        headers: {
+          Authorization: 'Basic aGVsbG9Ad29ybGQuY28tOkh1aFllYWgxMDE=',
+        },
+      },
+      (err, res, body) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.equal(401);
+        expect(JSON.parse(body).token).to.not.exist;
+        expect(JSON.parse(body)).to.deep.equal({ error: 'Unauthorized' });
+        done();
+      }
+    );
+  });
+
   it('GET /connect', (done) => {
     request.get(
       `${URL}/connect`,
@@ -53,6 +71,19 @@ describe('Auth Controller', () => {
     );
   });
 
+  it('GET /users/me with an invalid token', (done) => {
+    request.get(
+      `${URL}/users/me`,
+      { headers: { 'x-token': token + '5' } },
+      (err, res, body) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.equal(401);
+        expect(JSON.parse(body)).to.deep.equal({ error: 'Unauthorized' });
+        done();
+      }
+    );
+  });
+
   it('GET /users/me', (done) => {
     request.get(
       `${URL}/users/me`,
@@ -67,11 +98,24 @@ describe('Auth Controller', () => {
     );
   });
 
+  it('GET /disconnect with an invalid token', (done) => {
+    request.get(
+      `${URL}/disconnect`,
+      { headers: { 'x-token': token + '5' } },
+      (err, res, body) => {
+        expect(err).to.be.null;
+        expect(res.statusCode).to.equal(401);
+        expect(JSON.parse(body)).to.deep.equal({ error: 'Unauthorized' });
+        done();
+      }
+    );
+  });
+
   it('GET /disconnect', (done) => {
     request.get(
       `${URL}/disconnect`,
       { headers: { 'x-token': token } },
-      (err, res, body) => {
+      (err, res, _body) => {
         expect(err).to.be.null;
         expect(res.statusCode).to.equal(204);
         done();
